c9d9c744add999cd02eb84e29178561f6109dbf6
saurabh bhatt <saurabh7412@gmail.com>
2023-01-05 13:40:22 +0530

    prime number problem is done

---
+++ prime.js
@@ -0,0 +1,21 @@
+let num=13;
+
+let count=0;
+
+for( let i=1 ; i<=13 ; i++)
+{
+if(num%i===0)
+{
+   count++;
+}
+
+}
+if(count=2)
+{
+console.log("Prime");
+}
+else
+{
+console.log("Not Prime");
+
+}
